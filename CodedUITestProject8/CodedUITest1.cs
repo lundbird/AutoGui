9b073c669cfc631aa858259a00bc9c1a961b643f
alex <alex.lundberg@sandc.com>
2018-08-08 18:13:36 -0500

    initial commit

---
+++ CodedUITestProject8/CodedUITest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using System.Windows.Automation;
+
+namespace CodedUITestProject8
+{
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            GUILibrary runner = new GUILibrary();
+            runner.setWindow("SC IntelliShell - Select Connection Mode");
+            runner.Click("CANCEL");
+
+        }
+    }
+}
