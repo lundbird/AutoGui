ab93036330df4c566a935439244d02125c343d1b
alex <alex.lundberg@sandc.com>
2018-08-10 16:17:16 -0500

    update add package

---
+++ src/GUILibrary/bin/Debug/SimpleGUI.py
@@ -0,0 +1,19 @@
+import clr
+clr.AddReference(r"C:\Users\Alex.Lundberg\SimpleGUI\src\GUILibrary\bin\Debug\GUILibrary.dll")
+from GUILibrary import GUILibraryClass as gl
+
+class SimpleGUI:
+    def click(self,id):
+        gl.Click(id)
+    def write(self,value,id):
+        gl.Write(value,id)
+    def setWindow(self,id):
+        gl.setWindow(id)
+    def append(self,value,id):
+        gl.Append(value,id)
+    def rightclick(self,value,id):
+        gl.RightClick(value,id)
+    def sendkey(self,key):
+        gl.SendKey(key)
+    def read(self,id):
+        return gl.Read()
