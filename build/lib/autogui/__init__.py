efe9a8e029c37b8ef57381e1141b1f53d9a110da
alex <alex.lundberg@sandc.com>
2018-08-28 11:13:41 -0500

    put timeout parameter in init file so user can edit

--- build/lib/autogui/__init__.py
+++ build/lib/autogui/__init__.py
@@ -6,23 +6,24 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 clr.AddReference(dir_path + r'\src\GUILibrary\bin\Release\GUILibrary.dll')
 from GUILibrary import GUILibraryClass as gl
 
+timeout = 3
 #these are a complete copy-paste of the method definitions in the dll.
 #With IronPthon the dll methods can be used directly: from GUILibrary.GUILibraryClass import *
-def click(id,child=0,timeout=gl.timeout):
+def click(id,child=0,timeout=timeout):
     gl.Click(id,child,timeout)
-def write(value,id,child=0,timeout=gl.timeout):
+def write(value,id,child=0,timeout=timeout):
     gl.Write(value,id,child,timeout)
-def setWindow(id,contains=True,timeout=gl.timeout):
+def setWindow(id,contains=True,timeout=timeout):
     gl.setWindow(id,contains,timeout)
-def append(value,id,child=0,timeout=gl.timeout):
+def append(value,id,child=0,timeout=timeout):
     gl.Append(value,id,child,timeout)
-def rightclick(id,child=0,timeout=gl.timeout):
+def rightclick(id,child=0,timeout=timeout):
     gl.RightClick(id,child,timeout)
 def sendkey(key):
     gl.SendKey(key)
-def read(id,child=0,timeout=gl.timeout):
+def read(id,child=0,timeout=timeout):
     return gl.Read(id,child,timeout)
-def open(app,setActive=True,timeout=gl.timeout):
+def open(app,setActive=True,timeout=timeout):
     gl.Open(app,setActive)
 def close(window="activeWindow"):
     gl.Close(window)
