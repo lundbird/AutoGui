8a41057b8e8b89c3713ffb8f7bf04dd0c21bb946
alex lundberg <alex.lundberg@gmail.com>
2018-11-22 20:32:14 -0800

    rename projects to runner and tester

---
+++ autogui/src/Runner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using static GUILibrary.GUILibraryClass;
+
+public class Class1
+{
+    static void Main()
+    {
+        Console.Write(GetActiveWindow());
+        GUILibrary.GUILibraryClass.Open("notepad");
+        Console.Write(GUILibrary.GUILibraryClass.GetProperty("Edit","Name"));
+        Console.Write(GUILibrary.GUILibraryClass.GetProperty("File", "Id"));
+        Console.Write(GUILibrary.GUILibraryClass.GetProperty("Format", "class"));
+        Console.Write(GUILibrary.GUILibraryClass.Exists("Format"));
+        Console.Write(GUILibrary.GUILibraryClass.Exists("BadName"));
+
+    }
+}
+
+    
+
